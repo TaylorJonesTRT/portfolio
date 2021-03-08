6eca35aa5a982e87f20a668ed7d3e230c1c37a3f
Taylor Jones <taylor@taylorwjones.com>
2021-03-07 21:52:18 -0600

    Finished the mobile media query for css.

---
+++ src/components/Blog.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Blog = () => <h1>Coming Soon</h1>;
+
+export default Blog;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './styles/Index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
